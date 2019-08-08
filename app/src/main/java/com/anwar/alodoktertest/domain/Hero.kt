809dd1d8c110eda6d9e0c480a5ef17c19bf53877
Anwar Pasaribu <anwar_pasaribu@live.com>
2019-08-08 17:22:17 +0700

    Provide Home list data.

---
+++ app/src/main/java/com/anwar/alodoktertest/domain/Hero.kt
@@ -0,0 +1,5 @@
+package com.anwar.alodoktertest.domain
+
+class Hero(
+    val images: MutableList<String>
+)
--- app/src/main/java/com/anwar/alodoktertest/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/anwar/alodoktertest/ui/home/HomeViewModel.kt
@@ -2,9 +2,17 @@ package com.anwar.alodoktertest.ui.home
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.anwar.alodoktertest.domain.Hero
 
 class HomeViewModel : ViewModel() {
+
+    val heroList: LiveData<MutableList<Hero>> = MutableLiveData<Hero>().apply {
+        val heroList = MutableList<Hero>
+        heroList.add(listOf<>())
+        value = heroList
+    }
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
